fe565a5d5a7f40ebf3e65e783907fef4019467e6
r-emahh <re.m.a.kogi@gmail.com>
2026-06-02 19:17:56 +0900

    Test

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -5,6 +5,12 @@ public class InputManager : MonoBehaviour
 
     public static InputManager _inputManager { get; private set; }
 
+    void Awake()
+    {
+        
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
